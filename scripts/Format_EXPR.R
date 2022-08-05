d72c4f8a7659a7d90b51db6f5387fe6783091f6c
Minoru Nakano <minoru.nakano@gmail.com>
2022-08-05 14:09:48 -0400

    added log transformation

--- scripts/Format_EXPR.R
+++ scripts/Format_EXPR.R
@@ -12,5 +12,8 @@ expr = expr[,-1]
 case = read.csv( file.path(output_dir, "cased_sequenced.csv"), stringsAsFactors=FALSE , sep=";" )
 expr = expr[ , colnames(expr) %in% case[ case$expr %in% 1 , ]$patient ]
 
-write.table( expr , file= file.path(output_dir, "EXPR.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=TRUE )
+tpm <- (2 ^ expr) - 1
+tpm <- log2(tpm + 0.001)
+
+write.table( tpm , file= file.path(output_dir, "EXPR.csv") , quote=FALSE , sep=";" , col.names=TRUE , row.names=TRUE )
 
